c07ccd1b397e7cc2d92c93abab64b1aea85ab2fe
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2024-05-12 14:02:15 -0600

    large view

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,19 @@ const today = new Date();
 
 year.innerHTML = `${today.getFullYear()}`;
 date.innerHTML= `Last modification ${today.toLocaleString('en-US', { timeZoneName: 'short' })}`;
+
+function calculateWindChill(temp, windspeed) {
+    // console.log("temp:" + temp);
+    // console.log("windspeed:" + windspeed);
+    return (13.12 + 0.6215*temp - 11.37 * Math.pow(windspeed, 0.16) + 0.3965 * temp * Math.pow(windspeed, 0.16)).toFixed(2);
+}
+
+let temp = 32;
+let windspeed = 8;
+let windchill = "N/A";
+
+if (temp <= 10 && windspeed >= 4.8) {
+    windchill = calculateWindChill(temp, windspeed).toString() + "°C";
+}
+
+document.getElementById("windchill").textContent = windchill;
